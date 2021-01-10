f205b4ea48e089f7aed948a4b9c3879633a3cf7c
Gabriel Negrão <gabriel.negrao@iteris.com.br>
2021-01-10 19:36:07 -0300

    POST /orders

--- backend/src/main/java/com/devsuperior/dsdeliver/controllers/OrderController.java
+++ backend/src/main/java/com/devsuperior/dsdeliver/controllers/OrderController.java
@@ -5,9 +5,13 @@ import com.devsuperior.dsdeliver.services.OrderService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -21,5 +25,13 @@ public class OrderController {
     public ResponseEntity<List<OrderDTO>> findAll() {
         var list = service.findAll();
         return ResponseEntity.ok(list);
+    }
+
+    @PostMapping
+    public ResponseEntity<OrderDTO> insert(@RequestBody OrderDTO orderDTO) {
+        var order = service.insert(orderDTO);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{id}")
+                .buildAndExpand(order.getId()).toUri();
+        return ResponseEntity.created(uri).body(order);
     }
 }
