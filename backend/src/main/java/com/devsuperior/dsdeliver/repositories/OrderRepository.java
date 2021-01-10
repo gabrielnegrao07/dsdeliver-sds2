72042358fe3d3efb64ef95c875efc388d4375980
Gabriel Negrão <gabriel.negrao@iteris.com.br>
2021-01-10 19:14:01 -0300

    GET /orders

--- backend/src/main/java/com/devsuperior/dsdeliver/repositories/OrderRepository.java
+++ backend/src/main/java/com/devsuperior/dsdeliver/repositories/OrderRepository.java
@@ -1,9 +1,14 @@
 package com.devsuperior.dsdeliver.repositories;
 
 import com.devsuperior.dsdeliver.entities.Order;
+import com.devsuperior.dsdeliver.entities.OrderStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface OrderRepository extends JpaRepository<Order, Long> {
+
+    List<Order> findAllByStatusEqualsOrderByMoment(OrderStatus status);
 }
